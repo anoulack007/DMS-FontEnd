8e6db62bb1b83909c521eedc2fd79119df501e36
COVID-ALEX <Bounmastudent01@gmail.com>
2024-10-08 14:20:24 +0700

    update

--- src/pages/manage-user/index.tsx
+++ src/pages/manage-user/index.tsx
@@ -1,5 +1,22 @@
-const ManageUserPage = () => {
-  return <div>Manage-userPage</div>
+import DataTable from "./components/tableList"
+import Header from "./components/header";
+import { Box } from '@mui/material';
+
+function ManageUserPage() {
+    return (
+        <div>
+            <Header />
+            <div style={{marginTop: 100}}>name</div>
+            
+            
+            <Box sx={{ display: 'flex' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    
+                    <DataTable />
+                </Box>
+            </Box>
+        </div>
+    );
 }
 
 export default ManageUserPage
